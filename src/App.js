5ccae063b51bf4ff0972eae3b46e3ceb2ce14df1
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-05 23:01:50 -0400

    Defining removeContact function

--- src/App.js
+++ src/App.js
@@ -28,11 +28,17 @@ class App extends Component {
       }
     ]
   }
+    removeContact = (contact) => {
+        this.setState((state) => ({
+          contacts: state.contacts.filter((c) => c.id !== contact.id)
+        }))
+        
+    }
     render() {
     console.log('Props', 'this.props');
     return (
       <div>
-        <ListContacts contacts={this.state.contacts}/>
+        <ListContacts onDeleteContact={this.removeContact}   contacts={this.state.contacts}/>
       </div>
     )
   }
